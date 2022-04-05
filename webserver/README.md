4233a0018e0e69ce6489072b0c48e9c03c973c10
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-05 13:43:10 +0700

    act3 websevrer

--- webserver/README.md
+++ webserver/README.md
@@ -1,3 +1,5 @@
 # webservers:
 ## using flask
 see [act1](/webserver/act1/index.py)
+## apply html on act2
+see [act2](/webserver/act2/app.py)
---
+++ webserver/act3/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Markup, jsonify
+from datetime import datetime
+import PRi.GPIO as GPIO
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print("start webpage")
+    return render_template('index.html')
+
+
+@app.route('/updateTime')
+def updateTime():
+    print("update time")
+    now = datetime.datetime.now()
+    timeString = "TIME: " + now.strftime("%H:%M:%S")
+    return jsonify(timeupdate=timeString)
+
+
+@app.route('/<pin>/<action>')
+def LEDControl(pin, action):
+    print('Control LED')
+
+
+def GPIO_init(GPIO):
+    P_OUT = 3
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(P_OUT, GPIO.OUT)
+
+
+if __name__ == '__main__':
+    GPIO_init(GPIO)
+    app.run(debug=True, host='0.0.0.0', port=80)
